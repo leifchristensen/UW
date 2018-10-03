8c3234dd04742001285dbc35d1085976b0f7b0db
leifchristensen <leifc@live.com>
2018-10-02 17:46:53 -0700

    Proof of primitive default value inheritance

--- CSS 143/src/Hello.java
+++ CSS 143/src/Hello.java
@@ -1,11 +1,12 @@
 public class Hello {
 	public static Integer a;
+
 	public static void main(String[] args) {
-      
-      //int b;
-      int b = 0;
-      
-      System.out.print(a);
-      System.out.print(b);
-   }
+
+		// int b;
+		int b = 0;
+
+		System.out.print(a);
+		System.out.print(b);
+	}
 }
