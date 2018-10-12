a23ec38da0a8afba83319165742b4ac51ff9a0ed
leifchristensen <leifc@live.com>
2018-10-11 19:42:36 -0700

    Updates to Lab 3

--- CSS 143/src/HouseSales.java
+++ CSS 143/src/HouseSales.java
@@ -1,9 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class HouseSales {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		final int ARRAYLENGTH = 20;
+		String[] HouseAddress = new String[ARRAYLENGTH];
+		String[] HousePrice = new String[ARRAYLENGTH];
+		
+		Scanner input = new Scanner(new File("buyem.dat"));
+		
+		int counter = 0;
+		while(input.hasNextLine() || counter == ARRAYLENGTH) {
+			HouseAddress[counter] = input.nextLine();
+			HousePrice[counter] = input.next();
+		}
 
 	}
+	
+	
 
 }
