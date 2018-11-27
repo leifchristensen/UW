bcfdd8250bc6a5afa54e9479d5ac4f4171056089
Leif Christensen <leifc@live.com>
2018-11-27 14:09:02 -0800

    Created working isCorrect method for MA

--- src/QMultipleAnswer.java
+++ src/QMultipleAnswer.java
@@ -24,6 +24,26 @@ public class QMultipleAnswer extends QMultipleChoice{
 	public void setAnswers(ArrayList<String> answers2) {
 		this.answers = answers2;
 	}
+	
+	public boolean isCorrect(String toCompare) {
+		int numCorrect = 0;
+		//ArrayList<String> answers = new ArrayList<String>();
+		for(Character c : toCompare.toCharArray()) {
+			String test = c.toString();
+			if(!this.getAnswer().contains(test)) {
+				return false; //Returns false if any character in the response is not in the answer
+			}
+			else {
+				numCorrect++; // If there is a match, increment the number of matches by 1.
+			}
+		}
+		
+		if(numCorrect == this.getAnswer().length()) {
+			return true; // Only return true if the number of matches is equal to the number of answers
+		}
+		return false; // Return false if answer count numbers do not match
+		
+	}
 
 	
 
