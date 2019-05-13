cefb06c4d41b6f9775d46028798faac55439a46a
Leif Christensen <leifc@live.com>
2019-05-13 15:04:49 -0700

    Some more hash table.

--- MyHashTable.java
+++ MyHashTable.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class MyHashTable<K, V> {
 
 	int capacity;
+	ArrayList<K> keys;
 	ArrayList<V> values;
 	
 	public MyHashTable(int capacity) {
@@ -13,13 +14,12 @@ public class MyHashTable<K, V> {
 	}
 	
 	public void put(K searchKey, V newValue) {
-		if(searchKey == null || V == null) {
+		if(searchKey == null || newValue == null) {
 			throw new NullPointerException();
 		}
-		
-		
-		
-		values.set(hash(searchKey), newValue);
+		int index = hash(searchKey);
+		keys.set(index, searchKey);
+		values.set(index, newValue);
 	}
 	
 	public V get(K searchKey) {
@@ -27,9 +27,11 @@ public class MyHashTable<K, V> {
 	}
 	
 	private int hash(K key) {
-		int index;
-		while(values.get(index = System.identityHashCode(key) % capacity) != null) {
-			
+		int index = System.identityHashCode(key) % capacity;
+		// Return value of index of key if a key exists, or the first empty index where the key would go if the key does not exist.
+		while(!keys.get(index).equals(key) && keys.get(index) != null) {
+			// Breaks loop if the index is equal to an existing key or if there is a null key in an index before the key is found.
+			index = (index+1) % capacity;
 		}
 		// TODO:  takes a key and returns an int in the range [0...capacity]
 		return index;
